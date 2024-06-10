e24dcba0e2dc9fed11f9bb5b91ac2ebfa6701a31
Titoufeur <titouan.letondal@gmail.com>
2024-06-10 09:41:51 +0200

    import de demo.zip

---
+++ lib/main_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'master.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Master(),
+    );
+  }
+}
---
+++ lib/master.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Master extends StatelessWidget {
+  const Master({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
